503e881c6c7120ef4a000cb88f7a63aee982f506
mibers <woodrow.conran@gmail.com>
2026-02-04 15:49:20 +0000

    Refactor lexer with composable helper methods

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use std::fs;
+use std::env;
+
+use crate::ast::lexer::Lexer;
+
 mod ast;
 
 fn main() {
-    let input = "data wow()";
-
-    let lexer = ast::lexer::Lexer::new(input.to_string());
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        std::process::exit(1);
+    }
+    
+    let filename = &args[1];
+    
+    let input = fs::read_to_string(filename)
+        .unwrap_or_else(|err| {
+            eprintln!("Error reading file '{}': {}", filename, err);
+            std::process::exit(1);
+        });
+    
+    let lexer = Lexer::new(input);
     let tokens = lexer.tokenize();
-    println!("{:?}", tokens)
+    
+    for token in tokens {
+        println!("{:?}", token);
+    }
 }
